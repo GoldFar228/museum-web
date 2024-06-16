40ab9fe9b3003b1d02f0821bf1b617383986e56e
GoldFar <96344369+GoldFar228@users.noreply.github.com>
2024-06-16 15:02:50 +0500

    changed panel

--- museum/src/app/reusableComonents/panel/panel.component.ts
+++ museum/src/app/reusableComonents/panel/panel.component.ts
@@ -1,16 +1,47 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-panel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './panel.component.html',
   styleUrl: './panel.component.css'
 })
-export class PanelComponent {
-  router = inject(Router)
-  redirectTo(url: string){
-    this.router.navigate([url])
+export class PanelComponent implements OnInit {
+  activeItem: string = '';
+  constructor(private router: Router) {
+    // Подписываемся на события навигации
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Устанавливаем активный элемент на основе текущего маршрута
+        this.setActiveItem(this.router.url);
+      }
+    });
+  }
+
+  ngOnInit() {
+    // Устанавливаем активный элемент при инициализации компонента
+    this.setActiveItem(this.router.url);
+  }
+
+  redirectTo(url: string, item: string) {
+    this.activeItem = item;
+    this.router.navigate([url]).then(() => {
+      // Убедиться, что активный элемент установлен после завершения навигации
+      this.setActiveItem(this.router.url);
+    });
+  }
+  setActiveItem(url: string) {
+    if (url.includes('/map')) {
+      this.activeItem = 'map';
+    } else if (url.includes('/history')) {
+      this.activeItem = 'rules';
+    } else if (url.includes('/events')) {
+      this.activeItem = 'statistic';
+    } else {
+      this.activeItem = 'home';
+    }
   }
 }
